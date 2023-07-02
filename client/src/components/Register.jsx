3c522b867b6be8a3fa30f123bc7986e2ed818180
Ana Popovic <franjetina@gmail.com>
2023-07-02 13:36:44 +0200

    code cleanup

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -75,7 +75,6 @@ const Register = () => {
                   onChange={(e) => setPassword(e.target.value)}
                 ></input>
               </div>
-
               <div className="form-outline mb-4">
                 <input
                   className="form-control form-control-lg"
