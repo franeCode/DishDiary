b5ecc8fc93df12233e58eb205527090a35a82012
Ana Popovic <franjetina@gmail.com>
2023-07-07 18:02:39 +0200

    created new route which excludes rendering navbar in home, login and register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import "./App.css";
 import Navbar from './components/Navbar';
+import Menu from './components/Menu';
 import Register from './components/Register';
 import Home from './components/Home';
 import Login from './components/Login';
@@ -13,18 +14,41 @@ import CustomRecipes from './components/CustomRecipes';
 const App = () => {
   return (
     <Router>
-      <Navbar />
       <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/*"
+          element={<AppRoutes />}
+        />
+      </Routes>
+    </Router>
+  );
+};
+
+const AppRoutes = () => {
+  const location = useLocation();
+
+  const isHomePage = location.pathname === '/';
+
+  return (
+    <>
+      {isHomePage ? null : <Navbar />}
+      <Routes>
+        {/* <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} /> */}
+        <Route path="/menu" element={<Menu />} />
         <Route path="/recipe/:id" element={<RecipeView />} />
         <Route path="/recipes" element={<Recipes />} />
         <Route path="/cookbook" element={<CookBook />} />
         <Route path="/custom_recipes" element={<CustomRecipes />} />
         <Route path="/recipe_view" element={<RecipeView />} />
       </Routes>
-    </Router>
+    </>
   );
 };
 
