18eca98603df880b4608e46a7f54044307427cc4
Ana Popovic <franjetina@gmail.com>
2023-08-03 22:26:21 +0200

    font changed, icons added, book style improved

--- client/src/components/CookBook.jsx
+++ client/src/components/CookBook.jsx
@@ -41,19 +41,33 @@ const CookBook = () => {
         <div className='book-page relative'>
           <div className="lines"></div>
             <form className="list">
-              <input className='border-none fs-3' type="text" name='title' value={recipe.title} onChange={handleInputChange} placeholder='Title' />
-              <input className='flex word-wrap' type="text" name='ingredients' value={recipe.ingredients} onChange={handleInputChange} placeholder='Ingredients' />
-              <textarea className='border-none' type="text" name='instructions' value={recipe.instructions} onChange={handleInputChange} placeholder='Instructions' />
-              <button type="submit" onClick={handleSubmit}>Add Recipe
-              </button>
-              <button>
-                <Link to="/custom_recipes">View Recipes</Link>
-              </button>
+              <input className='border-none fs-3 w-75' type="text" name='title' value={recipe.title} onChange={handleInputChange} placeholder='Title' />
+              <input className='flex word-wrap w-75' type="text" name='ingredients' value={recipe.ingredients} onChange={handleInputChange} placeholder='Ingredients' />
+              <textarea className='border-none w-75' type="text" name='instructions' value={recipe.instructions} onChange={handleInputChange} placeholder='Instructions' />
             </form>
-            <div className="holes hole-top"></div>
+            <div className='d-flex flex-column gap-5 position-absolute top-50 end-0 translate-middle-y pe-5'>
+                <button className="btn text-white rounded">
+                <i style={{color: "#FF7D04"}} class="fa-solid fa-arrow-left fa-xl"></i>
+                </button>
+                <button className='btn text-white rounded' type="submit" onClick={handleSubmit}>
+                <i style={{color: "#FF7D04"}} class="fa-solid fa-check fa-xl"></i>
+                </button>
+                <button className='btn text-white rounded' type="submit" onClick={handleSubmit}>
+                <i style={{color: "#FF7D04"}} class="fa-regular fa-pen-to-square fa-xl"></i>
+                </button>
+                <button className='btn rounded bg-transparent'>
+                  <Link className='text-decoration-none text-white' to="/custom_recipes">
+                    <i style={{color: "#FF7D04"}} class="fa-regular fa-eye fa-xl"></i>
+                  </Link>
+                </button>
+              </div>
+              {/* <div className="position-absolute top-0">
+                <i class="fa-solid fa-left-long"></i>
+              </div> */}
+              <div className="holes hole-top"></div>
               <div className="holes hole-middle"></div>
               <div className="holes hole-bottom"></div>
-        </div>
+          </div>
         
         </div>
         
