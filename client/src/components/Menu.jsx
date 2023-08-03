78d82bd982c50d8e2d79460e4ac804a77eef068e
Ana Popovic <franjetina@gmail.com>
2023-08-03 22:28:25 +0200

    layout organized differently, button style changed

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import img_write from "../assets/img/make-food.jpg";
 import view from "../assets/img/view.jpg";
+
 import logo from '../assets/img/logo-icon.svg';
 
 const Menu = () => {
@@ -9,10 +10,10 @@ const Menu = () => {
         <div className="container d-flex flex-column align-items-center" style={{ height: "100vh", marginTop: "10rem", fontFamily: "'Poppins', sans-serif" }}>
             <div className="d-flex flex-column justify-content-center align-items-center p-5 mb-5 fs-5 shadow">
                 <h1 className="d-flex align-items-center justify-content-center fs-1 gap-3 mb-5">Welcome to 
-                    <span className="logo">
-                        <span>Recipes</span>
-                        <img src={logo} alt="logo"></img>
-                    </span>
+                <div className="logo">
+                    <span>DishDiary</span>
+                    <img src={logo} alt="logo"></img>
+                </div>
                 </h1>
                 <p className="text-center">
                 Get ready to embark on a culinary adventure where you can unleash your creativity and explore a world of flavors. Whether you're a seasoned chef or a passionate home cook, this app is designed to inspire and simplify your cooking experience.
@@ -29,7 +30,7 @@ const Menu = () => {
                             <div class="card-body p-3 ms-3">
                                 <h5 class="card-title fs-2">Ready to share your secret recipes with the world?</h5>
                                 <p class="card-text mt-5 fs-5">Start writing and let your imagination run wild! Jot down your unique creations, cherished family recipes, or even your innovative twists on classic dishes. Don't worry, we've got you covered with a simple and intuitive recipe editor that ensures your culinary masterpieces are captured flawlessly.</p>
-                                <button className="fs-3 px-4 mt-3 float-end rounded">
+                                <button style={{backgroundColor: "#FF7D04"}} className="btn fs-3 px-4 mt-3 float-end rounded">
                                     <Link to="/cookbook" style={{ textDecoration: "none", color: "#fff"}}>Write</Link>
                                 </button>
                             </div>
@@ -43,7 +44,7 @@ const Menu = () => {
                             <div class="card-body">
                                 <h5 class="card-title fs-2">Craving some culinary inspiration or looking to broaden your repertoire? </h5>
                                 <p class="card-text mt-5 fs-5">Dive into a treasure trove of delightful recipes contributed by fellow food enthusiasts from all around the globe. Discover an endless array of appetizing dishes, ranging from mouthwatering main courses to decadent desserts. The possibilities are as vast as your appetite!</p>
-                                <button className="fs-3 px-4 mt-3 rounded float-end">
+                                <button style={{backgroundColor: "#FF7D04"}} className="btn fs-3 px-4 mt-3 rounded float-end">
                                     <Link to="/cookbook" style={{ textDecoration: "none", color: "#fff"}}>View</Link>
                                 </button>
                             </div>
@@ -54,11 +55,12 @@ const Menu = () => {
                     </div>
                 </div>
             </div>
-            <div className="d-flex flex-column p-5 fs-5">
+            <div className="d-flex flex-column justify-content-center align-items-center p-5 mt-5 fs-5 shadow">
                 <p className="text-center">
                 No matter which path you choose, we're here to fuel your passion for cooking and connect you with a community of fellow food lovers. So, grab your apron, sharpen those knives, and let's dive into the wonderful world of flavors together!
                 </p>
-                <p className="text-center fs-4 fw-bold">Are you ready? Let's cook up something extraordinary!</p>
+                <p className="text-center fs-4 fw-bold">Are you ready? Let's cook up something extraordinary!
+                </p>
             </div>
         </div>
         </div>
