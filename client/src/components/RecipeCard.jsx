6bc160c08a857ae4754d5eff3ca8059a6a5db955
Ana Popovic <franjetina@gmail.com>
2023-09-30 15:28:05 +0200

    styling improved

--- client/src/components/RecipeCard.jsx
+++ client/src/components/RecipeCard.jsx
@@ -59,44 +59,9 @@ const RecipeCard = ({ recipe, setRecipes, showIcon, customRecipeId }) => {
     <div className="col-sm-6 col-md-4 col-lg-3 mt-5">
       <div
         className="card align-items-center rounded-4 shadow-sm mb-5 position-relative"
-        style={{ width: "18rem", height: "25rem", borderColor: "transparent" }}
+        style={{ width: "18rem", height: "20rem", borderColor: "transparent", backgroundColor: "rgba(201, 200, 206)" }}
       >
-        {displayRecipe && displayRecipe.image_url ? (
-          <img
-            src={displayRecipe.image_url}
-            className="card-img-top rounded-top-4"
-            alt="image"
-          />
-        ) : (
-          <img
-            src={noImage}
-            className="card-img-top rounded-top-4"
-            alt="image"
-          />
-        )}
-        <div className="card-body pt-0">
-          <h5 className="card-title fs-4">
-            {displayRecipe ? displayRecipe.title : "No title available"}
-          </h5>
-          <p className="fs-6 overflow-hidden mb-0" style={{ height: "1.5rem" }}>
-            INGREDIENTS:{" "}
-            <span style={{ fontSize: "0.8rem" }}>
-              {/* {displayRecipe ? displayRecipe.measure : ""} */}
-              {displayRecipe ? displayRecipe.ingredients : ""}
-            </span>
-          </p>
-          <p className="d-flex justify-content-start align-items-center gap-4 card-text">
-            <button
-              className="btn bg-transparent rounded"
-              type="button"
-              onClick={() => handleReadMore(recipe.id)}
-            >
-              <i
-                className="fa-solid fa-info fa-lg"
-                style={{ color: "#FF7D04" }}
-              ></i>
-            </button>
-            {showIcon && (
+        {showIcon && (
               <button
                 className="btn rounded bg-transparent position-absolute top-0 end-0 p-2"
                 onClick={() => shareRecipe(customRecipeId, recipe.id)}
@@ -112,6 +77,42 @@ const RecipeCard = ({ recipe, setRecipes, showIcon, customRecipeId }) => {
                 </Link>
               </button>
             )}
+        {displayRecipe && displayRecipe.image_url ? (
+          <img
+            src={displayRecipe.image_url}
+            className="card-img-top rounded-top-4 pt-4"
+            alt="image"
+            style={{width: "14rem", height: "13rem"}}
+          />
+        ) : (
+          <img
+            src={noImage}
+            className="card-img-top rounded-top-4 pt-4"
+            alt="image"
+            style={{width: "14rem", height: "13rem"}}
+          />
+        )}
+        <div className="card-body pt-3">
+          <h5 className="card-title fs-4">
+            {displayRecipe ? displayRecipe.title : "No title available"}
+          </h5>
+          {/* <p className="fs-6 overflow-hidden mb-0" style={{ height: "1.5rem" }}>
+            INGREDIENTS:{" "}
+            <span style={{ fontSize: "0.8rem" }}>
+              {displayRecipe ? displayRecipe.ingredients : ""}
+            </span>
+          </p> */}
+          <p className="d-flex justify-content-start align-items-center gap-4 card-text position-absolute bottom-0">
+            <button
+              className="btn bg-transparent rounded"
+              type="button"
+              onClick={() => handleReadMore(recipe.id)}
+            >
+              <i
+                className="fa-solid fa-info fa-lg"
+                style={{ color: "var(--text-color)" }}
+              ></i>
+            </button>
             {showIcon && (
               <button
                 className="btn text-white rounded"
@@ -124,15 +125,12 @@ const RecipeCard = ({ recipe, setRecipes, showIcon, customRecipeId }) => {
                 >
                   <i
                     className="fa-solid fa-trash fa-lg"
-                    style={{ color: "#FF7D04" }}
+                    style={{ color: "var(--text-color)" }}
                   ></i>
                 </Link>
               </button>
             )}
-            {/* <button>
-              <a href={`https://www.youtube.com/embed/${recipe.youtube_link}`}><i className="fa-brands fa-youtube fs-2" style={{color: "#FF7D04"}}></i>
-              </a>
-            </button> */}
+            {/* <span>Written by: {displayRecipe.user_id}</span> */}
           </p>
         </div>
       </div>
