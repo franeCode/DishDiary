cd12a8a31b7185f722ae6319457b3027f2ae8700
Ana Popovic <franjetina@gmail.com>
2023-10-09 16:58:12 +0200

    search input added in recipe component

--- client/src/components/Recipes.jsx
+++ client/src/components/Recipes.jsx
@@ -1,42 +1,73 @@
 import React, { useEffect, useState } from "react";
 import RecipeCard from "./RecipeCard";
 import axios from "axios";
-import { Link } from "react-router-dom";
-import { handleSubmit } from "./Api";
-import { useMemo } from "react";
-import { useCallback } from "react";
 
 const Recipes = () => {
   const [recipes, setRecipes] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const recipesPerPage = 4;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredRecipes, setFilteredRecipes] = useState([]);
 
   useEffect(() => {
-    const fetchRecipes = async () => {
-      try {
-        const response = await axios.get(
-          "http://localhost:5000/api/get_recipes",
-          {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("access_token")}`,
-            },
-          }
-        );
-        const data = response.data;
-    
-        if (Array.isArray(data)) {
-          setRecipes(data);
-          console.log("Response data:", data);
-        } else {
-          console.error("Invalid data format:", data);
-        }
-      } catch (error) {
-        console.error("Error fetching recipes:", error);
-      }
-    };    
-    
     fetchRecipes();
   }, []);
+
+  const fetchRecipes = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:5000/api/get_recipes",
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("access_token")}`,
+          },
+        }
+      );
+      const data = response.data;
+
+      if (Array.isArray(data)) {
+        setRecipes(data);
+        setFilteredRecipes(data);
+        console.log("Response data:", data);
+      } else {
+        console.error("Invalid data format:", data);
+      }
+    } catch (error) {
+      console.error("Error fetching recipes:", error);
+    }
+  };
+
+  const handleSearchChange = (e) => {
+    const query = e.target.value.toLowerCase();
+    setSearchQuery(query);
+    console.log("Search query:", query);
+
+    // Ensure recipes is not undefined before filtering
+    if (Array.isArray(recipes)) {
+      const filtered = recipes.filter((recipe) => {
+        if (recipe.title) {
+          return recipe.title.toLowerCase().includes(query);
+        }
+        return false;
+      });
+      setFilteredRecipes(filtered);
+      console.log("Filtered:", filtered);
+    }
+  };
+
+  useEffect(() => {
+    if (Array.isArray(recipes)) {
+      const filtered = recipes.filter(
+        (recipe) =>
+          recipe.title && recipe.title.toLowerCase().includes(searchQuery)
+      );
+      setFilteredRecipes(filtered);
+    }
+  }, [recipes, searchQuery]);
+
+  useEffect(() => {
+    handleSearchChange({ target: { value: searchQuery.toLowerCase() } });
+  }, [recipes, searchQuery]);
 
   // Calculate the index of the last recipe to display on the current page
   const indexOfLastRecipe = currentPage * recipesPerPage;
@@ -44,15 +75,6 @@ const Recipes = () => {
   const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
   // Get the recipes to display on the current page
   const currentRecipes = recipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
-
-  // const currentRecipes = useMemo(() => {
-  //   // Calculate currentRecipes here
-  //   const indexOfLastRecipe = currentPage * recipesPerPage;
-  //   const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
-  //   return recipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
-  // }, [currentPage, recipes, recipesPerPage]);
-  
-
   // Function to handle going to the next page
   const nextPage = () => {
     setCurrentPage(currentPage + 1);
@@ -64,16 +86,6 @@ const Recipes = () => {
       setCurrentPage(currentPage - 1);
     }
   };
-  // const nextPage = useCallback(() => {
-  //   setCurrentPage(currentPage + 1);
-  // }, [currentPage]);
-  
-  // const prevPage = useCallback(() => {
-  //   if (currentPage > 1) {
-  //     setCurrentPage(currentPage - 1);
-  //   }
-  // }, [currentPage]);
-  
 
   return (
     <div>
@@ -95,46 +107,36 @@ const Recipes = () => {
           ></div>
           <div className="mx-5 mt-3">
             <div className="w-100 d-flex flex-row justify-content-between align-items-center p-2">
-              {/* <button className="btn text-dark rounded">
-                <Link className="text-decoration-none text-white px-2" to="/menu">
-                  <i
-                    style={{ color: "#FF7D04" }}
-                    className="fa-solid fa-arrow-left fa-xl"
-                  ></i>
-                </Link>
-                Go back
-              </button> */}
-              <div className="fs-3">COOKBOOK</div>
-              <button
-                className="btn text-white rounded"
-              >
-                <Link
-                  className="text-decoration-none text-white"
-                  to="/create_recipe"
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Search recipes"
+                  value={searchQuery}
+                  onChange={(e) => handleSearchChange(e)}
+                />
+                <button
+                  className="btn btn-outline-secondary text-light"
+                  type="button"
+                  id="button-addon2"
                 >
-                  <i
-                    style={{ color: "#FF7D04" }}
-                    className="fa-regular fa-pen-to-square fa-xl"
-                  ></i>
-                </Link>
-              </button>
+                  Search
+                </button>
+              </div>
             </div>
           </div>
           <ul className="row row-cols-lg-2 row-cols-md-1 list-unstyled p-5 my-5 p-sm-2">
-            {Array.isArray(recipes) ? (
-              currentRecipes.map((recipe) => (
+            {(searchQuery === "" ? currentRecipes : filteredRecipes).map(
+              (recipe) => (
                 <RecipeCard
                   key={recipe.id}
                   recipe={recipe}
                   setRecipes={setRecipes}
                   showIcon={false}
                 />
-              ))
-            ) : (
-              <p>Loading...</p>
+              )
             )}
           </ul>
-          {/* <div className="position-relative"> */}
           <div className="position-absolute bottom-0 start-50 translate-middle-x">
             <button className="bg-transparent p-3" onClick={() => prevPage()}>
               <i
@@ -148,7 +150,6 @@ const Recipes = () => {
                 style={{ color: "#414448" }}
               ></i>
             </button>
-            {/* </div> */}
           </div>
         </div>
       </div>
